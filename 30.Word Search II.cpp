6d9ddef7666b12af41d5ff457bc60b6764c048ed
Sneh Sagar <sneh.sagar98@gmail.com>
2020-07-01 13:46:01 +0530

    Day-30

---
+++ 30.Word Search II.cpp
@@ -0,0 +1,101 @@
+/*
+
+
+Given a 2D board and a list of words from the dictionary, find all words in the board.
+
+Each word must be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once in a word.
+
+ 
+
+Example:
+
+Input: 
+board = [
+  ['o','a','a','n'],
+  ['e','t','a','e'],
+  ['i','h','k','r'],
+  ['i','f','l','v']
+]
+words = ["oath","pea","eat","rain"]
+
+Output: ["eat","oath"]
+ 
+
+Note:
+All inputs are consist of lowercase letters a-z.
+The values of words are distinct.
+
+
+*/
+
+struct TrieNode {
+    string word { "" };
+    array<TrieNode*, 26> chars { nullptr };
+};
+
+class Solution {
+public:
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+        vector<string> result;
+        
+        // insert words to trie
+        TrieNode* root = new TrieNode();
+        for (const auto& word : words) {
+            auto it = root;
+            for (auto c : word) {
+                int idx = c - 'a';
+                if (it->chars[idx] == nullptr) {
+                    it->chars[idx] = new TrieNode();
+                }
+                it = it->chars[idx];
+            }
+            it->word = word;
+        }
+
+        for (int i = 0; i < board.size(); ++i) {
+            for (int j = 0; j < board[i].size(); ++j) {
+                int idx = board[i][j] - 'a'; // convert char to idx
+                if (root->chars[idx] != nullptr) {
+                    search(&board, &result, i, j, root);
+                }
+            }
+        }
+
+        return result;
+    }
+private:
+    void search(vector<vector<char>>* board_ptr, vector<string>* result_ptr, int i, int j, TrieNode* node) {
+        // dereference pointers (avoiding non-const references)
+		auto& board = *board_ptr;
+        auto& result = *result_ptr;
+
+        int idx = board[i][j] - 'a'; // convert char to idx
+        node = node->chars[idx];
+        if (node) {
+            if (!empty(node->word)) {
+				// if this is a leaf node (word exists) insert to result and unset it to avoid duplicates
+                result.emplace_back(node->word);
+                node->word = "";
+            }
+
+            // helper function to check for valid direction
+            auto IsValidDir = [&board](int i, int j) -> bool {
+                return i >= 0 && i < board.size()
+                    && j >= 0 && j < board[i].size()
+                    && board[i][j] != '*';
+            };
+
+			// we need to make letters as visited
+            char letter = board[i][j];
+            board[i][j] = '*';
+			
+			// check adjacent cells. we can do this in a loop, but this is faster
+            if (IsValidDir(i - 1, j)) search(board_ptr, result_ptr, i - 1, j, node);
+            if (IsValidDir(i + 1, j)) search(board_ptr, result_ptr, i + 1, j, node);
+            if (IsValidDir(i, j - 1)) search(board_ptr, result_ptr, i, j - 1, node);
+            if (IsValidDir(i, j + 1)) search(board_ptr, result_ptr, i, j + 1, node);
+
+            board[i][j] = letter; // unmark visited
+        }
+    }
+};
